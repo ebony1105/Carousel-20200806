fa88be4a0bb3d15451f2f062031bd071a517bc9e
ebony1105 <45974321+ebony1105@users.noreply.github.com>
2020-08-14 17:11:06 +0800

    无缝轮播的实现
    集体跳绳

--- main.js
+++ main.js
@@ -1,51 +1,49 @@
-var allButtons = $('#buttons > span')
-
-for (let i = 0; i < allButtons.length; i++) {
-    $(allButtons[i]).on('click', function(x) {
-        var index = $(x.currentTarget).index() //jQuery 封装好的方法可以直接获取这个元素是第几个儿子
-        var n = index * (-300)
-        $('#images').css({
-            transform: 'translateX(' + n + 'px)'
-        })
-        n = index
-        activeButton(allButtons.eq(n))
+let n;
+init();
+setInterval(( )=> {
+    makeLeave(getImage(n)).one('transitionend',function(e){
+        makeEnter($(e.currentTarget))
     })
+    makeCurrent(getImage(n+1))
+     n += 1;
+},3000);
+
+function getImage(n) {
+    return $(`.images > img:nth-child(${x(n)})`);
 }
 
-var n = 0;
-var size = allButtons.length
-playSlide(n % size)
-//这个方法会将找到的DOM对象自动封装成jQuery对象
-
-
-//console.log(n%4) //那么就会从0~3进行循环
-
-var timerId = setTimer()
-
-$('.window').on('mouseenter', function() {
-    window.clearInterval(timerId)
-})
-
-
-$('.window').on('mouseleave', function() {
-    timerId = setTimer()
-})
-
-function playSlide(index) {
-    allButtons.eq(index).trigger('click')
-}
-
-function setTimer()
+function x(n)
 {
-    return setInterval(() => {
-        n += 1
-        playSlide(n % size)
-    }, 1000)
+    if(n > 4)
+    {
+        n = n % 4
+        if( n === 0)
+        {
+            n =4
+        }
+    }  // n = 1,2,3,4
+    return n;
 }
 
-function activeButton($button) {
-    $button
-        .addClass('red')
-        .siblings('.red')
-        .removeClass('red')
+function init()
+{
+    n = 1;
+    $(`.images > img:nth-child(${n})`).addClass('current').siblings().addClass('enter');
+}
+
+function makeCurrent($node)
+{
+    $node.removeClass('enter leave').addClass('current');   //一次性删除两个类
+    return $node;
+}
+
+function makeLeave($node){   // makeLeave返回的是一个undefined
+    $node.removeClass('enter current').addClass('leave');
+    return $node;//不写这个的话 那么上面的one这个API将会找不到
+}
+
+
+function makeEnter($node){
+    $node.removeClass('leave current').addClass('enter');
+    return $node;
 }
